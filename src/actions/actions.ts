569053a98be96314a7c2e0a879df0d6207b75b54
Bőti Marcell <de9d6m@inf.elte.hu>
2024-09-19 02:37:41 +0200

    init

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -1,16 +1,25 @@
 "use server";
 
 import { createRoom } from "@/lib/server-utils";
+import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
 export async function createRandomGame() {
   try {
     console.log("CREATE RANDOM GAME FUNC");
-    
-    await createRoom(1).then((room) => {
-      console.log("CREATE RANDOM GAME FUNC 2");
+
+    const room = await createRoom(1);
+    console.log("CREATE RANDOM GAME FUNC 2");
+
+    if (room) {
+      console.log("CREATE RANDOM GAME FUNC 3");
+      console.log(room);
+
+
+      revalidatePath('/room');
       redirect(`/room/${room.id}`);
-    });
+    }
+
 
   } catch (e) {
     return {
